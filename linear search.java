02cda2b54187f93aa44c3669627ad0f9cb13837c
dheekshi16 <dheekshiprabhu483@gmail.com>
2026-09-23 16:18:15 +0530

    Add files via upload

---
+++ linear search.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        //Scanner sc=new Scanner(System.in);
+        int arr[]=new int[5];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        System.out.println(arr[0]);
+        
+        int arr2[]={10,20,30,40};
+         System.out.println(arr[1]);
+    }
+}
