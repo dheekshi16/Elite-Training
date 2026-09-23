5985f88ca3773194c3919fc5526596576118e194
dheekshi16 <dheekshiprabhu483@gmail.com>
2026-09-23 16:14:23 +0530

    Add files via upload

---
+++ multiply pattern.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=i;j<=i;j++){
+                if(j%2==0){
+                    System.out.print("");
+                }
+                else{
+                    System.out.print(j+"*");
+                }
+                
+            }
+             
+
+        }
+
+    }
+} 1*3*5
