84af71af78e0b9aed867f9db3cd78ec8f5c681a3
dheekshi16 <dheekshiprabhu483@gmail.com>
2026-09-23 16:03:45 +0530

    Add files via upload

---
+++ subarray.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int[] arr=new int[a];
+		for(int i=0;i<a;i++){
+		    arr[i]=sc.nextInt();
+		}
+		int tar=sc.nextInt();
+		for(int k=0;k<a;k++){
+		    
+    		for(int i=k;i<a;i++){
+         		int sum=0,j;
+    		    for(j=k;j<=i;j++){
+    		    System.out.print(arr[j]+" ");
+    		    sum+=arr[j];
+    		   
+    		    }
+    		    if(tar==sum){
+    		      System.out.print("SUM"+" "+(i-1)+" "+(j-1));
+    		    }
+    		    System.out.println();
+    		}
+    		 System.out.println();
+		}
+	}
+}
